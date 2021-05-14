5f63bcfed060a610b12e36e93a4d89a0ef16edfb
Leo23-23 <ando33161@gmail.com>
2021-05-14 16:07:26 +0200

    la fonction get_rdv_client de la case 2 est opérationnel

--- src/main/java/com/fges/ckonsoru/Gestion_BDD.java
+++ src/main/java/com/fges/ckonsoru/Gestion_BDD.java
@@ -89,17 +89,49 @@ public class Gestion_BDD {
         }
     }
 
+    
+    
+    
     //je voudrai une fonction qui affiche les rendez vous pris d' un client en donnant à la fonction le nom du client.
     public void Get_client_rdv (String Nom){
         try { 
             connexion = DriverManager.getConnection(this.jdbcUrl, this.username,this.password);
             System.out.println("connected");
-            System.out.println("Bonjour "+ Nom );
+            System.out.println("Voici la liste des rdv de "+ Nom );
             /*connexion.close();*/
         }
         catch(SQLException e){ 
             System.out.println("erreur de connexion a la base de donnée");
             e.printStackTrace();
+        }
+        try {
+            
+            String my_request = "SELECT rv_id, rv_debut, rv_client, vet_nom" +
+                                " FROM rendezvous" +
+                                " INNER JOIN veterinaire ON rendezvous.vet_id = veterinaire.vet_id" +
+                                " WHERE rv_client = '" + Nom + "'" +
+                                " ORDER BY rv_debut DESC;";
+            
+            PreparedStatement statement = connexion.prepareStatement(my_request);
+            ResultSet res = statement.executeQuery();
+            
+            
+            Integer compteur = 0; // pour qu on puisse savoir si il y a eu plus de 0 resultat trouvé
+            
+            while (res.next()) {
+                System.out.println(res.getString(1) + ": " + res.getString(2) + " avec " + res.getString(4) );
+                compteur++;
+            }
+            // a la fin de la boucle si compteur est toujour = 0 => pas de resultat
+            if (compteur == 0) {
+                System.out.println("Le client n' a pas pris de rendez vous.");
+            }
+            System.out.println( compteur + " resultat(s) trouvés ");
+            connexion.close();
+           
+            
+        } catch (SQLException e) {
+            System.out.println("Une erreur s' est produit pendant pour afficher les rendez vous du client : " + e);
         }
     }
 }
