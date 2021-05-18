98744be5c1c06384a11f90cfe9840de08f03e5c4
Leo23-23 <ando33161@gmail.com>
2021-05-18 13:51:31 +0200

    le delai marche maintenant, objectif suivant conditionné l' insertion dans table annulation

--- src/main/java/com/fges/ckonsoru/Gestion_time.java
+++ src/main/java/com/fges/ckonsoru/Gestion_time.java
@@ -1,43 +1,55 @@
 package com.fges.ckonsoru;
-import java.sql.Time;
-import java.time.LocalTime;
-import java.util.*;
-import java.lang.Math;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 public class Gestion_time {
-    public int Get_hour ( LocalTime time1, String time2){
-        Date date1 = Time.valueOf(time1);
-        Date date2 = Time.valueOf(time2);
-        System.out.println(date1);
-        System.out.println(date2);
-        //GregorianCalendar cal = new GregorianCalendar();
-       // cal.setTime(date2);
-        int finH = date2.getHours(); // finH = 10
-        int debutH = date1.getHours(); //debutH = 8
-        int result_hour = finH - debutH;
-        return Math.abs(result_hour) ;
-    }
-    public int Get_minute ( LocalTime time1, String time2){
-        Date date1 = Time.valueOf(time1);
-        Date date2 = Time.valueOf(time2);
-        //GregorianCalendar cal = new GregorianCalendar();
-        //cal.setTime(date2);
-        int finM = date2.getMinutes(); // finH = 10
-        //cal.setTime(date1);    
-        int debutM = date1.getMinutes(); //debutH = 8
-        int result_minute = finM - debutM;
-        return Math.abs(result_minute);
-    }
-    public int Get_seconde ( LocalTime time1, String time2){
-        Date date1 = Time.valueOf(time1);
-        Date date2 = Time.valueOf(time2);
-        //GregorianCalendar cal = new GregorianCalendar();
-        //cal.setTime(date2);
-        int finS = date2.getSeconds(); // finH = 10
-        //cal.setTime(date1);
-        int debutS = date1.getSeconds(); //debutH = 8
-        int result_second = finS - debutS;
-        return Math.abs(result_second);
-    }
+   public Date dateStr_to_Date ( String date) throws ParseException{
+    Date date1=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(date);
+    return date1;
+   }
+   public LocalDateTime dateDate_to_LocalDateTime(Date date){
+    LocalDateTime date2 = new java.sql.Timestamp(date.getTime()).toLocalDateTime();
+    return date2;
+   }
+   public long Duration_2_LocalDateTime_to_SECOND (LocalDateTime date_debut , LocalDateTime date_fin){
+    long diff = ChronoUnit.SECONDS.between(date_debut,  date_fin);
+    return diff;
+   }
+   public long Get_Duration_Hour(long diff_hour){
+       return diff_hour /3600%24;
+   }
+   public long Get_Duration_Minutes(long diff_hour){
+       return diff_hour /60%60;
+   }
+   public long Get_Duration_Seconds(long diff_hour){
+       return diff_hour %60;
+   }
+   public String date_heure ( String date , String heure){
+       return date +" "+heure;
+   }
+   public String hh_mm_ss (long heure, long minute, long second){
+       return heure+":"+minute+":"+second;
+   }
+
+   public String  traitement_date(LocalDateTime now, String date)throws ParseException {
+       long dif_heure;
+       long dif_minute;
+       long dif_second;
+       // je commence par transformer la date d' annulation en date
+   Date madate1= this.dateStr_to_Date(date);
+   //je vais transformer cette date en localdatetime
+   LocalDateTime madate2 = this.dateDate_to_LocalDateTime(madate1);
+   //je vais faire la difference entre mes 2 localdatetime
+   long diff_date = this.Duration_2_LocalDateTime_to_SECOND(now, madate2);
+   //je prends chaque difference : 
+   dif_heure = this.Get_Duration_Hour(diff_date);
+   dif_minute = this.Get_Duration_Minutes(diff_date);
+   dif_second = this.Get_Duration_Seconds(diff_date);
+
+   return this.hh_mm_ss(dif_heure, dif_minute, dif_second);
+   }
     
 }
